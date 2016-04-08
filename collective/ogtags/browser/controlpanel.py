e18a10fb79d198fc24916677d6a0551968c8f921
Maurits van Rees <maurits@vanrees.org>
2016-04-08 14:30:35 +0200

    Made translation strings unicode.
    Might not matter, but I'm used to this.

--- collective/ogtags/browser/controlpanel.py
+++ collective/ogtags/browser/controlpanel.py
@@ -6,33 +6,33 @@ from zope.interface import Interface
 
 class IOGTagsControlPanel(Interface):
     enabled = schema.Bool(
-        title=_('Enabled'),
+        title=_(u'Enabled'),
         default=True)
 
     og_site_name = schema.TextLine(
-        title=_('Site name'),
+        title=_(u'Site name'),
         required=False,
         default=u'')
 
     fb_username = schema.TextLine(
-        title=_('Facebook username'),
+        title=_(u'Facebook username'),
         required=False,
         default=u'')
 
     fb_id = schema.TextLine(
-        title=_('Facebook app id'),
+        title=_(u'Facebook app id'),
         required=False,
         default=u'')
 
     tw_id = schema.TextLine(
-        title=_('Twitter username'),
-        description=_('example: @zestsoftware'),
+        title=_(u'Twitter username'),
+        description=_(u'example: @zestsoftware'),
         required=False,
         default=u'')
 
     default_img = schema.TextLine(
-        title=_('Default image'),
-        description=_('Path to default image'),
+        title=_(u'Default image'),
+        description=_(u'Path to default image'),
         required=False,
         default=u'/logo.png')
 
